592966b7fa75dd56b558c99ba92d81b959457cea
Tedy Tri Saputro <tedy@saputro.dev>
2024-07-03 14:36:38 +0700

    application.properties

--- src/main/java/com/subrutin/quarkus/learningconfig/GreetingResource.java
+++ src/main/java/com/subrutin/quarkus/learningconfig/GreetingResource.java
@@ -1,7 +1,5 @@
 package com.subrutin.quarkus.learningconfig;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -10,16 +8,9 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    private final String message;
-
-    public GreetingResource(@ConfigProperty(name="app.welcome-message") String message) {
-        this.message = message;
-    }
-    
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return this.message;
+        return "Hello from Quarkus REST";
     }
 }
